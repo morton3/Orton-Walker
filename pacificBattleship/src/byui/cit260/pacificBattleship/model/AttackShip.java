b18fa0566f58a26f95a35886378a9ef06bc00723
morton3 <matt.orton72@gmail.com>
2016-10-07 17:15:26 -0600

    10-07-2016 Added my half of the classes

---
+++ pacificBattleship/src/byui/cit260/pacificBattleship/model/AttackShip.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pacificBattleship.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author ort09
+ */
+public class AttackShip implements Serializable{
+    
+    private String shipType;
+    private int attackRange;
+    private int attackDamage;
+
+    public AttackShip() {
+    }
+
+    public String getShipType() {
+        return shipType;
+    }
+
+    public void setShipType(String shipType) {
+        this.shipType = shipType;
+    }
+
+    public int getAttackRange() {
+        return attackRange;
+    }
+
+    public void setAttackRange(int attackRange) {
+        this.attackRange = attackRange;
+    }
+
+    public int getAttackDamage() {
+        return attackDamage;
+    }
+
+    public void setAttackDamage(int attackDamage) {
+        this.attackDamage = attackDamage;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.shipType);
+        hash = 97 * hash + this.attackRange;
+        hash = 97 * hash + this.attackDamage;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AttackShip other = (AttackShip) obj;
+        if (this.attackRange != other.attackRange) {
+            return false;
+        }
+        if (this.attackDamage != other.attackDamage) {
+            return false;
+        }
+        if (!Objects.equals(this.shipType, other.shipType)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "AttackShip{" + "shipType=" + shipType + ", attackRange=" + attackRange + ", attackDamage=" + attackDamage + '}';
+    }
+    
+    
+    
+}
--- pacificBattleship/src/pacificbattleship/PacificBattleship.java
+++ pacificBattleship/src/pacificbattleship/PacificBattleship.java
@@ -7,6 +7,12 @@ package pacificbattleship;
 
 import byui.cit260.pacificBattleship.model.Game;
 import byui.cit260.pacificBattleship.model.Character;
+import byui.cit260.pacificBattleship.model.Player;
+import byui.cit260.pacificBattleship.model.Upgrade;
+import byui.cit260.pacificBattleship.model.Ship;
+import byui.cit260.pacificBattleship.model.Transport;
+import byui.cit260.pacificBattleship.model.AttackShip;
+import byui.cit260.pacificBattleship.model.ShipAbility;
 
 /**
  *
@@ -49,5 +55,97 @@ public class PacificBattleship {
         String characterInfo = characterOne.toString();
         System.out.println(characterInfo);
         
+        /********************************************************
+         * Player
+         ********************************************************/
+        
+        Player playerOne = new Player();
+        
+        playerOne.setName("Matthew");
+        playerOne.setTime(5.00);
+        
+        String playerInfo = playerOne.toString();
+        System.out.println(playerInfo);
+        
+        /********************************************************
+         * Upgrade
+         ********************************************************/
+        
+        Upgrade battleshipAttack = new Upgrade();
+        
+        battleshipAttack.setUpgradeName("Extra Battleship Gun");
+        battleshipAttack.setUpgradeType("scrap");
+        battleshipAttack.setUpgradeCurrentLevel(0);
+        battleshipAttack.setUpgradeMaxLevel(3);
+        battleshipAttack.setUpgradeCurrency(1);
+        
+        String upgradeInfo = battleshipAttack.toString();
+        System.out.println(upgradeInfo);
+        
+        /********************************************************
+         * Ship
+         ********************************************************/
+        
+        Ship battleship0 = new Ship();
+        
+        battleship0.setName("USS Tennessee");
+        battleship0.setDescription("The lead ship of her class of battleship,"
+                + " was the third ship of the United States Navy named in honor"
+                + " of the 16th US state.[5] During World War II in the Pacific"
+                + " Theater, she was damaged during the attack on Pearl Harbor"
+                + " in December 1941 but was repaired and modernized. She"
+                + " participated in shore bombardments at the Aleutian Islands,"
+                + " Tarawa, the Marshall Islands, the Marianas, the"
+                + " Philippines, Iwo Jima, Okinawa among others. She was also"
+                + " involved in the Battle of Surigao Strait, the final"
+                + " battleship vs. battleship conflict ever.\n"
+                + "\n"
+                + "After the end of World War II, Tennessee was placed on"
+                + " reserve in the \"mothball fleet\" for nearly 15 years"
+                + " before finally being scrapped in 1959. - Wikipedia");
+        battleship0.setShipType("battleship");
+        battleship0.setShipSunk(false);
+        battleship0.setMapSymbol("ƃ");
+        
+        String shipInfo = battleship0.toString();
+        System.out.println(shipInfo);
+        
+        /********************************************************
+         * Transport
+         ********************************************************/
+        
+        Transport transport0 = new Transport();
+        
+        transport0.setTroopsRemaining(5);
+        
+        String transportInfo = transport0.toString();
+        System.out.println(transportInfo);
+        
+        /********************************************************
+         * Attack Ship
+         ********************************************************/
+        
+        AttackShip battleship = new AttackShip();
+        
+        battleship.setShipType("Battleship");
+        battleship.setAttackRange(1);
+        battleship.setAttackDamage(2);
+        
+        String battleshipInfo = battleship.toString();
+        System.out.println(battleshipInfo);
+        
+        /********************************************************
+         * Ship Ability
+         ********************************************************/
+        
+        ShipAbility aircraftCarrier = new ShipAbility();
+        
+        aircraftCarrier.setAbility("planes");
+        aircraftCarrier.setActive(true);
+        aircraftCarrier.setCount(1);
+        
+        String shipAbilityInfo = aircraftCarrier.toString();
+        System.out.println(shipAbilityInfo);
+
     }   
 }
