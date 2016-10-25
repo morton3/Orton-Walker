7af4a9295e3ec4b539649b6d95c3f8dc6b9bbc2e
morton3 <matt.orton72@gmail.com>
2016-10-25 12:00:24 -0600

    10-25-2016 Command Menu update

--- pacificBattleship/src/byui/cit260/pacificBattleship/view/CommandMenu.java
+++ pacificBattleship/src/byui/cit260/pacificBattleship/view/CommandMenu.java
@@ -4,12 +4,118 @@
  * and open the template in the editor.
  */
 package byui.cit260.pacificBattleship.view;
+import byui.cit260.pacificBattleship.model.Location;
 
 /**
  *
  * @author ort09
  */
 public class CommandMenu {
+    
+    Location A01 = new Location();
+    ;
+    
+    Location A02 = new Location();
+    Location A03 = new Location();
+    Location A04 = new Location();
+    Location A05 = new Location();
+    Location A06 = new Location();
+    Location A07 = new Location();
+    Location A08 = new Location();
+    Location A09 = new Location();
+    Location A10 = new Location();
+    Location B01 = new Location();
+    Location B02 = new Location();
+    Location B03 = new Location();
+    Location B04 = new Location();
+    Location B05 = new Location();
+    Location B06 = new Location();
+    Location B07 = new Location();
+    Location B08 = new Location();
+    Location B09 = new Location();
+    Location B10 = new Location();
+    Location C01 = new Location();
+    Location C02 = new Location();
+    Location C03 = new Location();
+    Location C04 = new Location();
+    Location C05 = new Location();
+    Location C06 = new Location();
+    Location C07 = new Location();
+    Location C08 = new Location();
+    Location C09 = new Location();
+    Location C10 = new Location();
+    Location D01 = new Location();
+    Location D02 = new Location();
+    Location D03 = new Location();
+    Location D04 = new Location();
+    Location D05 = new Location();
+    Location D06 = new Location();
+    Location D07 = new Location();
+    Location D08 = new Location();
+    Location D09 = new Location();
+    Location D10 = new Location();
+    Location E01 = new Location();
+    Location E02 = new Location();
+    Location E03 = new Location();
+    Location E04 = new Location();
+    Location E05 = new Location();
+    Location E06 = new Location();
+    Location E07 = new Location();
+    Location E08 = new Location();
+    Location E09 = new Location();
+    Location E10 = new Location();
+    Location F01 = new Location();
+    Location F02 = new Location();
+    Location F03 = new Location();
+    Location F04 = new Location();
+    Location F05 = new Location();
+    Location F06 = new Location();
+    Location F07 = new Location();
+    Location F08 = new Location();
+    Location F09 = new Location();
+    Location F10 = new Location();
+    Location G01 = new Location();
+    Location G02 = new Location();
+    Location G03 = new Location();
+    Location G04 = new Location();
+    Location G05 = new Location();
+    Location G06 = new Location();
+    Location G07 = new Location();
+    Location G08 = new Location();
+    Location G09 = new Location();
+    Location G10 = new Location();
+    Location H01 = new Location();
+    Location H02 = new Location();
+    Location H03 = new Location();
+    Location H04 = new Location();
+    Location H05 = new Location();
+    Location H06 = new Location();
+    Location H07 = new Location();
+    Location H08 = new Location();
+    Location H09 = new Location();
+    Location H10 = new Location();
+    Location I01 = new Location();
+    Location I02 = new Location();
+    Location I03 = new Location();
+    Location I04 = new Location();
+    Location I05 = new Location();
+    Location I06 = new Location();
+    Location I07 = new Location();
+    Location I08 = new Location();
+    Location I09 = new Location();
+    Location I10 = new Location();
+    Location J01 = new Location();
+    Location J02 = new Location();
+    Location J03 = new Location();
+    Location J04 = new Location();
+    Location J05 = new Location();
+    Location J06 = new Location();
+    Location J07 = new Location();
+    Location J08 = new Location();
+    Location J09 = new Location();
+    Location J10 = new Location();
+    
+    
     public void commandMenu(){
         System.out.println(        "\n       A       B       C       D       E       F       G       H       I       J    "
  + "\n┌--┬-------┬-------┬-------┬-------┬-------┬-------┬-------┬-------┬-------┬-------┐"
