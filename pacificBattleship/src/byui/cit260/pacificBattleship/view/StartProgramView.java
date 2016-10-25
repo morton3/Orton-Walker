d7bd90d366cad950400da66c8d17094a39c33f77
Nathan37127 <wal14052@byui.edu>
2016-10-25 13:27:25 -0600

    Merge origin/master
    Conflicts:
    	pacificBattleship/src/byui/cit260/pacificBattleship/model/AttackShip.java

--- pacificBattleship/src/byui/cit260/pacificBattleship/view/StartProgramView.java
+++ pacificBattleship/src/byui/cit260/pacificBattleship/view/StartProgramView.java
@@ -22,9 +22,17 @@ public class StartProgramView {
 
     private void displayBanner() {
         System.out.println(
-        "\n*************************************************************"
-        +"\n*                                                          *"
-                
+        "\n********************************************************************************************************"
+        +"\n*Its December 7, 1941 and the Japanese have just bombed Pearl Harbor, the United States Naval*"
+        +"\n*base in the Pacific. Causing the United States to be forced into the global conflict of World War 2.*"
+        +"\n*In this role playing activity you start out as a one-star general. As you advance against the enemy*"
+        +"\n*you will be promoted throughout the game topping out as a 5-star general after the Japanese*"
+        +"\n*have surrendered. Based on your knowledge of past events you will pick and choose what forces*"
+        +"\n*are needed for what battle. But watch out and don’t make the wrong move or make the wrong*"
+        +"\n*guess of the intent of the Japanese military’s next move. It may lead your forces into defeat and*"
+        +"\n*you will be decommissioned thus losing the game.*"
+        +"\n*                                                                                                     *"
+        +"\n*******************************************************************************************************"
         );
                 
     }
