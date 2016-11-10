97a210f6500c6c67aabb0b8ca4269632fa6c4b15
Nathan37127 <wal14052@byui.edu>
2016-11-10 13:10:46 -0700

    11-10-16 nathan

--- pacificBattleship/src/byui/cit260/pacificBattleship/view/BattleResultView.java
+++ pacificBattleship/src/byui/cit260/pacificBattleship/view/BattleResultView.java
@@ -9,6 +9,44 @@ package byui.cit260.pacificBattleship.view;
  *
  * @author Nathan
  */
-public class BattleResultView {
+public class BattleResultView extends View{
+    
+       public BattleResultView() {
+        super("\n"
+                   + "\n-------------------------------------------------"
+                   +"\n| Battle Results                                     |"
+                   +"\n--------------------------------------------------"
+                   +"\nA - Attack again?"
+                   +"\nQ - Quit"
+                   +"\n--------------------------------------------------");
+              }
+    
+         @Override
+    public boolean doAction(String value) {
+      value = value.toUpperCase();
+      
+      switch (value){
+          case "A":
+              this.attackView();
+              break;
+         
+          default:
+              System.out.println("\n*** Invalid selection *** Try again");
+              break;
+      }
+      
+       return false;
+    }
+
+    private void attackView() {
+       AttackMenuView attackMenu = new AttackMenuView();
+        attackMenu.display();
+    }
+    private void results() {
+        
+         System.out.println("\n*** Battle results *** Displayed");
+        
+    }
+
     
 }
