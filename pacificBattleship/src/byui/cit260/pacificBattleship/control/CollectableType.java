8a9c9241a1468ab4e653e3cc89e50655d5f77c41
Nathan37127 <wal14052@byui.edu>
2016-11-22 13:25:15 -0700

    11-22-16 added collectable type

---
+++ pacificBattleship/src/byui/cit260/pacificBattleship/control/CollectableType.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pacificBattleship.control;
+
+/**
+ *
+ * @author Nathan
+ */
+public enum CollectableType {
+    nukePart1("First Nuclear Bomb Part"),
+    nukePart2("Second Nuclear Bomb Part"),
+    nukePart3("Third Nuclear Bomb Part"),
+    nukePart4("Forth Nuclear Bomb Part"),
+    nukePart5("Fith Nuclear Bomb Part"),
+    nukePart6("Sixth Nuclear Bomb Part"),
+    nukePart7("Seventh Nuclear Bomb Part"),
+    nukePart8("Eighth Nuclear Bomb Part"),
+    nukePart9("Ninth Nuclear Bomb Part"),
+    nukePart10("Tenth Nuclear Bomb Part"),
+    nukePart11("Eleventh Nuclear Bomb Part"),
+    nukePart12("Twelfth Nuclear Bomb Part"),
+    nukePart13("Thirteenth Nuclear Bomb Part"),
+    nukePart14("Fourteenth Nuclear Bomb Part"),
+    nukePart15("Fifteenth Nuclear Bomb Part");
+
+    private String description;
+    
+    
+    private CollectableType(String description) {
+        this.description = description;
+    }
+    public String getDescription() {
+        return description;
+    }
+    
+
+}
+
+
